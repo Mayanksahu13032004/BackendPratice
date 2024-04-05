4889e1164795558622e36a4cfbc24b0c36c2b470
Mayanksahu13032004 <mayanksahu13032004.com>
2024-04-05 07:56:54 +0530

    Add structure changes

---
+++ BackFront/Backend/server.js
@@ -0,0 +1,47 @@
+import  express  from "express";
+
+const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.send('server is ready');
+// });
+
+// getta list of five jokes
+
+app.get('/api/jokes',(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'A third joke',
+            content:'This is a third joke'
+        },
+        {
+            id:4,
+            title:'A fourth joke',
+            content:'This is a fourth joke'
+        },
+        {
+            id:5,
+            title:'A fiveth joke',
+            content:'This is a fivth joke'
+        },
+    ];
+    res.send(jokes);
+})
+
+
+const port=process.env.PORT||3000;
+
+app.listen(port,()=>{
+    console.log(`server at http://localhost:${port}`);
+});
